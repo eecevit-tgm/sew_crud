68350f0dab0d60f9bd8fb647d4500d055a18ade5
eecevit-tgm <eecevit@student.tgm.ac.at>
2018-10-19 13:10:15 +0200

    endlich mal was neues

--- src/main/python/server/server.py
+++ src/main/python/server/server.py
@@ -1,3 +1,13 @@
 import sqlite3
 
 conn = sqlite3.connect('test.db')
+
+c = conn.cursor()
+
+#erstellen der DB
+c.execute('''CREATE TABLE user
+             (id id, username text, email text)''')
+
+conn.commit()
+
+conn.close()
