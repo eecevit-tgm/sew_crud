2ce094f6d08eb05ea80e3c603a8753a2e9170868
eecevit-tgm <eecevit@student.tgm.ac.at>
2018-12-03 21:19:39 +0100

    changes

--- src/unittest/python/GK/test_read.py
+++ src/unittest/python/GK/test_read.py
@@ -11,4 +11,9 @@ def client():
 
 def test_readall(client):
     res = client.get('/user')
+    assert res.json == [{"username": "eecevit", "email": "eecevit@student.tgm.ac.at", "picture": "ds"},{"username":  "dsunaric", "email":  "dsunaric@student.tgm.ac.at", "picture": "link"}]
+
+
+def test_singleUser(client):
+    res = client.get('/user/eecevit')
     assert res.json == [{"username": "eecevit", "email": "eecevit@student.tgm.ac.at", "picture": "ds"}]
